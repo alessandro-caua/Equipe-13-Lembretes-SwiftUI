8f1f66f03e7a58ba0c212a615f21133a49374ce6
Alessandro Cauã Macedo Marciel <70543246+alessandro-caua@users.noreply.github.com>
2025-02-09 15:05:01 -0300

    Delete Equipe-13-Lembretes-SwiftUI/Equipe 13 Lembretes APP directory

--- Equipe-13-Lembretes-SwiftUI/Equipe 13 Lembretes APP/Equipe 13 Lembretes APP/Models/UsuarioModel.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-
-struct UsuarioModel {
-    var nome: String
-    var imagem: Image
-}
--- Equipe-13-Lembretes-SwiftUI/Equipe 13 Lembretes APP/Equipe 13 Lembretes APP/ViewModels/UsuarioViewModel.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-
-class UsuarioViewModel: ObservableObject {
-    @Published var nomeUsuario: String = "Nome do Usuário"
-    @Published var imagemPerfil: Image = Image(systemName: "person.crop.circle.fill")
-}
