0fecfce5b93b6fcfc17cfdf580ce6cb17982ef18
Ian Pacini <124642552+IanPacini123@users.noreply.github.com>
2025-02-12 21:25:23 -0300

    Criei uma tab bar customizada, a logica é complexa, nao sei se vamos manter.

--- Equipe-13-Lembretes-SwiftUI/Equipe 13 Lembretes APP/Equipe 13 Lembretes APP/TesteEquipe13APP.swift
+++ Equipe-13-Lembretes-SwiftUI/Equipe 13 Lembretes APP/Equipe 13 Lembretes APP/TesteEquipe13APP.swift
@@ -4,7 +4,20 @@ import SwiftUI
 struct TesteEquipe13App: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            TabView {
+                Tab {
+                    Text("1")
+                } label: {
+                    Label("Home1", systemImage: "house.fill")
+                }
+                
+                Tab {
+                    Text("2")
+                } label: {
+                    Label("Home2", systemImage: "house")
+                }
+
+            }
         }
     }
 }
